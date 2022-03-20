24c1a7064e3c1e3380a81e31f96b19032da17253
VictorCreciun <85232431+VictorCreciun@users.noreply.github.com>
2022-03-20 00:12:35 -0700

    modified env file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./API/server");
 
 //POST CONST
-const PORT = 1234;
+const PORT = 1234 || process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`Server is live on http://localhost:${PORT}`);
